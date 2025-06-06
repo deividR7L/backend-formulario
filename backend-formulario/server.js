22e6725e5bcef8ed02e152bfd3acd5024022d4f7
Deivid Rodrigues Mendes <davimelgasso7l@icloud.com>
2025-06-05 21:26:31 -0300

    altercaçao e configuraçao

--- backend-formulario/server.js
+++ backend-formulario/server.js
@@ -39,4 +39,6 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
